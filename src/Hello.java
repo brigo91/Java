fcc39fdee3624e4a752bc03ae64ee5599e353943
brigo <brigovaczbalazs@hotmail.com>
2024-03-11 12:29:19 +0100

    If

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,10 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Balázs!");
-        System.out.println("Hello, World");
+        boolean isAlien = true;
+        if (!isAlien) {
+            System.out.println("It is not an alien");
+            System.out.println("And I am scared of aliens");
+        }
     }
 }

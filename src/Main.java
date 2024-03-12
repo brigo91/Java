e32a3c67e8faf43fe37fd190e775c914dcf96452
brigo <brigovaczbalazs@hotmail.com>
2024-03-12 21:30:02 +0100

    Advanced switch

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        printDayOfWeek(0);
+        printDayOfWeek(1);
+        printDayOfWeek(2);
+        printDayOfWeek(3);
+        printDayOfWeek(4);
+        printDayOfWeek(5);
+        printDayOfWeek(6);
+        printDayOfWeek(7);
+    }
+
+    public static void printDayOfWeek(int day) {
+        String dayOfWeek = switch(day){
+            case 0 -> {yield "Sunday";}
+            case 1 -> "Monday";
+            case 2 -> "Tuesday";
+            case 3 -> "Wednesday";
+            case 4 -> "Thursday";
+            case 5 -> " Friday";
+            case 6 -> "Saturday";
+            default -> "Not valid day";
+        };
+        System.out.println(day + "stands for" + dayOfWeek);
+    }
+}

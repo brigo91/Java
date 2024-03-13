566ba8b19a4469e86167045c03ab7863de5b7271
brigo <brigovaczbalazs@hotmail.com>
2024-03-13 13:47:59 +0100

    Input from console (terminal only)

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        printDayOfWeek(0);
-        printDayOfWeek(1);
-        printDayOfWeek(2);
-        printDayOfWeek(3);
-        printDayOfWeek(4);
-        printDayOfWeek(5);
-        printDayOfWeek(6);
-        printDayOfWeek(7);
+        int currentYear = 2022;
+
+        System.out.println(getInputFromConsole(currentYear));
+
+        System.out.println(getInputFromScanner(currentYear));
     }
 
-    public static void printDayOfWeek(int day) {
-        String dayOfWeek = switch(day){
-            case 0 -> {yield "Sunday";}
-            case 1 -> "Monday";
-            case 2 -> "Tuesday";
-            case 3 -> "Wednesday";
-            case 4 -> "Thursday";
-            case 5 -> " Friday";
-            case 6 -> "Saturday";
-            default -> "Not valid day";
-        };
-        System.out.println(day + "stands for" + dayOfWeek);
+    public static String getInputFromConsole(int currentYear) {
+
+        String name = System.console().readLine("Hi, What's your Name? ");
+        System.out.println("Hi " + name + ", Thanks for taking the course!");
+
+        String dateOfBirth = System.console().readLine("What year were you born? ");
+        int age = currentYear - Integer.parseInt(dateOfBirth);
+
+        return "So you are " + age + " years old";
+    }
+
+    public static String getInputFromScanner(int currentYear) {
+
+        return "";
     }
 }

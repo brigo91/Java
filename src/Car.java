1d81189fa7c4060d8d5796eed7c1e1171f013799
brigo <brigovaczbalazs@hotmail.com>
2024-03-13 17:07:36 +0100

    Getters generate

--- src/Car.java
+++ src/Car.java
@@ -1,9 +1,30 @@
 public class Car {
-    private String make;
-    private String model;
-    private String color;
-    private int doors;
-    private boolean convertible;
+    private String make = "Tesla";
+    private String model = "Model X";
+    private String color = "Gray";
+    private int doors = 2;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    private boolean convertible = true;
 
     public void describeCar() {
 

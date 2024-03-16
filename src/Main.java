cf0a51fe84e84e2e94abcb2b0c9868ee190d7d68
brigo <brigovaczbalazs@hotmail.com>
2024-03-16 17:25:40 +0100

    Challenge part2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        Item coke = new Item("drink", "coke", 1.50);
+        /*Item coke = new Item("drink", "coke", 1.50);
         coke.printItem();
         coke.setSize("LARGE");
         coke.printItem();
 
         Item avocado = new Item("Topping", "avocado", 1.50);
-        avocado.printItem();
+        avocado.printItem();*/
+
+        /*Burger burger = new Burger("regular", 4.00);
+        burger.addToppings("BACON", "CHEESE", "MAYO");
+        burger.printItem();*/
+        /*MealOrder regularMeal = new MealOrder();
+        regularMeal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+        regularMeal.setDrinkSize("LARGE");
+        regularMeal.printItemizedList();*/
+        MealOrder secondMeal = new MealOrder("turkey", "7-up",
+                "chili");
+        secondMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
+        secondMeal.setDrinkSize("SMALL");
+        secondMeal.printItemizedList();
     }
 }

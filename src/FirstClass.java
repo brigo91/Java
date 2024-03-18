e0bd436461a55486df3cf3687ff75ee83dd44ef9
brigo <brigovaczbalazs@hotmail.com>
2024-03-18 18:50:24 +0100

    ArrayList

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class FirstClass {
+    record GroceryItem(String name, String type, int count){
+        public GroceryItem(String name){
+            this(name, "DAIRY", 1);
+        }
+    }
     public static void main(String[] args) {
-        System.out.print("Hello, Balázs!");
+        GroceryItem[] groceryArray = new GroceryItem[3];
+        groceryArray[0] = new GroceryItem("milk");
+        groceryArray[1] = new GroceryItem("apples", "PRODUCE", 6);
+        groceryArray[2] = new GroceryItem("oranges", "PRODUCE", 5);
+        System.out.println(Arrays.toString(groceryArray));
+
+        ArrayList<GroceryItem> groceryList = new ArrayList<>();
+        groceryList.add(new GroceryItem("Butter"));
+
     }
 }

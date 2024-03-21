07d92c98af674925fcf727ef1230dd48fd66ee3b
brigo <brigovaczbalazs@hotmail.com>
2024-03-21 16:36:43 +0100

    Interfaces par2

--- src/dev/lpa/Animal.java
+++ src/dev/lpa/Animal.java
@@ -1,6 +1,8 @@
 package dev.lpa;
 
 interface FlightEnabled {
+    double MILES_TO_KM = 1.60934;
+    double KM_TO_MILES = 0.621371;
     void takeOff();
     void land();
     void fly();
---
+++ src/dev/lpa/Jet.java
@@ -0,0 +1,22 @@
+package dev.lpa;
+
+public class Jet implements Trackable, FlightEnabled{
+    public void takeOff() {
+        System.out.println(getClass().getSimpleName() + " is taking off");
+    }
+
+    @Override
+    public void land() {
+        System.out.println(getClass().getSimpleName() + " is landing");
+    }
+
+    @Override
+    public void fly() {
+        System.out.println(getClass().getSimpleName() + " is flying");
+    }
+
+    @Override
+    public void track() {
+        System.out.println(getClass().getSimpleName() + "'s coordinates recorded");
+    }
+}

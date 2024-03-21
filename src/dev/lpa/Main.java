ac108694203f89fbdeea6f8acece9252ae100442
brigo <brigovaczbalazs@hotmail.com>
2024-03-21 13:45:35 +0100

    Interfaces par1

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,28 +1,20 @@
 package dev.lpa;
 
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        Dog dog = new Dog("Wolf", "big", 100 );
-        dog.makeNoise();
-        doAnimalStuff(dog);
+        Bird bird = new Bird();
+        Animal animal = bird;
+        FlightEnabled flier = bird;
+        Trackable tracked = bird;
 
-        ArrayList<Animal> animals = new ArrayList<>();
-        animals.add(dog);
-        animals.add(new Dog("German Shepard", "big", 150));
-        animals.add(new Fish("Goldfish", "small", 1));
-        animals.add(new Fish("Barracuda", "big", 75));
-        animals.add(new Dog("Pug", "small", 20));
+        animal.move();
+//        flier.move();
+//        tracked.move();
 
-        animals.add(new Horse("Clydesdale", "large", 1000));
-        for (Animal animal : animals) {
-            doAnimalStuff(animal);
-        }
-    }
+        flier.takeOff();
+        flier.fly();
+        tracked.track();
+        flier.land();
 
-    private static void doAnimalStuff(Animal animal){
-        animal.makeNoise();
-        animal.move("slow");
     }
 }

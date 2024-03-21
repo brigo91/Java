30a8db7deae37e8c97bd9a239f3e55118719b4aa
brigo <brigovaczbalazs@hotmail.com>
2024-03-21 21:48:53 +0100

    Interfaces part3

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,5 +1,8 @@
 package dev.lpa;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Bird bird = new Bird();
@@ -20,6 +23,20 @@ public class Main {
 
         Trackable truck = new Truck();
         truck.track();
+
+        LinkedList<FlightEnabled> fliers = new LinkedList<>();
+        fliers.add(bird);
+
+        List<FlightEnabled> betterFliers = new LinkedList<>();
+        betterFliers.add(bird);
+
+        triggerFliers(fliers);
+        flyFliers(fliers);
+        landFliers(fliers);
+
+        triggerFliers(betterFliers);
+        flyFliers(betterFliers);
+        landFliers(betterFliers);
     }
     private static void inFlight(FlightEnabled flier){
         flier.takeOff();
@@ -28,5 +45,23 @@ public class Main {
             tracked.track();
         }
         flier.land();
+    }
+
+    private static void triggerFliers(List<FlightEnabled> fliers){
+        for (var flier: fliers){
+            flier.takeOff();
+        }
+    }
+
+    private static void flyFliers(List<FlightEnabled> fliers){
+        for (var flier: fliers){
+            flier.fly();
+        }
+    }
+
+    private static void landFliers(List<FlightEnabled> fliers){
+        for (var flier: fliers){
+            flier.land();
+        }
     }
 }
